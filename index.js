516dd6c335f512335b53a7edf48554ed83cac475
Tim Burmester <Tim-Burmester1@web.de>
2022-07-19 23:56:18 +0200

    3.8: changed cors policies

--- index.js
+++ index.js
@@ -14,8 +14,29 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 
 //import cors
+//choose between general course (seen first option here, but not recommended or recommended version below)
+
+/*
 const cors = require('cors');
 app.use(cors());
+*/
+
+
+//recommended cors policies
+const cors = require('cors');
+let allowedOrigins =  ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://my-awesome-site123.netlify.app'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 //import import express validator 
 const { check, validationResult } = require('express-validator');
